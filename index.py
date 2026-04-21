df9e86f78e76d41de84eb35c9cd32146e452fa95
siddharth-oz <krishforsiddhu@gmail.com>
2026-04-21 15:13:38 +0530

    intial commit

--- index.py
+++ index.py
@@ -2,12 +2,13 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-df = pd.read_csv('/content/yourfile.csv')
-df.head()
-df.info()
-df.describe()
+df = pd.read_csv("IMDB Dataset.csv")
+print(df.head())
+print(df.columns)
+print(df.info())
+print(df.describe())
 highest_votes = df[df['Votes'] == df['Votes'].max()]
-highest_votes
+print(highest_votes)
 sns.barplot(x='Genre', y='Votes', data=df)
 plt.xticks(rotation=90)
 plt.show()
